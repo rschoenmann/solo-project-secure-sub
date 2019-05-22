cd109a9cfc818ebc1f902cbe4bd7deacf131fbe2
Rachel Schoenmann <rachel.schoenmann@gmail.com>
2019-05-22 14:35:51 -0500

    added authentication and authorization for base mode

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
-    console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('is authenticated?', req.isAuthenticated());
+    console.log('secret req.user:', req.user);
+    let queryText = 'SELECT * FROM "secret" WHERE "secrecy_level" <= $1;';
+    pool.query(queryText, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
